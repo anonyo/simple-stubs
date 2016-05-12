aa1c7120d7257c9d54ab151bc277e059adb1af93
Anonyo Hossain <anonyo.hossain@gmail.com>
2016-05-12 17:12:24 -0400

    Class method

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 require "post"
 
 describe Post do
-  describe "#today" do
+  describe ".today" do
     it "returns posts created today" do
       posts_today = double("posts_today")
       allow(Post).to receive(:today).and_return posts_today
